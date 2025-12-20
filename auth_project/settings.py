8c8647b15b1cf227683b8f1350349dfb88a7a908
Ankush Patil <172409761+ankushp1650@users.noreply.github.com>
2025-12-20 11:47:46 +0100

    Add EMAIL_TIMEOUT setting for email configuration

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -78,7 +78,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_TIMEOUT = 10  # seconds
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
