2833444c95a5a210406cf179604368489702a183
Ankush Patil <172409761+ankush-patil-tech@users.noreply.github.com>
2026-01-24 13:57:35 +0100

    Refactor run_sentiment_background to use user_id

--- auth_app/utils/sentiment_runner.py
+++ auth_app/utils/sentiment_runner.py
@@ -1,21 +1,30 @@
+
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 from auth_app.services.news_fetcher import store_news_for_user
 from auth_app.services.news_processing import process_news_for_user
 from auth_app.models import UserSentimentStatus
 
 
-def run_sentiment_background(user):
+def run_sentiment_background(user_id):
     try:
+        user = User.objects.get(id=user_id)
+
         status, _ = UserSentimentStatus.objects.get_or_create(user=user)
         status.status = "RUNNING"
+        status.started_at = timezone.now()
         status.save()
 
         store_news_for_user(user)
         process_news_for_user(user)
 
         status.status = "DONE"
+        status.finished_at = timezone.now()
         status.save()
 
     except Exception as e:
         status.status = "FAILED"
+        status.error_message = str(e)
         status.save()
-        print("Sentiment error:", e)
+        print("🔥 Sentiment error:", e)
